0be0c88c8dc963a041221271ef1d7d4a5496264b
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-14 11:16:31 -0500

    Future Fixing

--- src/app/(pages)/form/consentForm/page.jsx
+++ src/app/(pages)/form/consentForm/page.jsx
@@ -37,35 +37,88 @@ export default function FormPage() {
     const [postal, setPostal] = useState('');
     const [parentBirthday, setParentBirthday] = useState('');
 
+
     useEffect(() => {
-        if (typeof window !== 'undefined') {
-            // Retrieve data from local storage
-            const storedType = localStorage.getItem('accountType');
-            setType(storedType);
-            const storedBirthday = localStorage.getItem('birthday');
-            setBirthday(storedBirthday);
-            const storedFirstName = localStorage.getItem('firstName');
-            setFirstName(storedFirstName);
-            const storedLastName = localStorage.getItem('lastName');
-            setLastName(storedLastName);
-            const storedPhone = localStorage.getItem('phone');
-            setPhone(storedPhone);
-            const storedEmail = localStorage.getItem('email');
-            setEmail(storedEmail);
-            const storedAddressOne = localStorage.getItem('addressOne');
-            setAddressOne(storedAddressOne);
-            const storedAddressTwo = localStorage.getItem('addressTwo');
-            setAddressTwo(storedAddressTwo);
-            const storedPostal = localStorage.getItem('postal');
-            setPostal(storedPostal);
-            if(storedType === "Child"){
-                const storedParentBirthday = localStorage.getItem('parentBirthday');
-                setParentBirthday(storedParentBirthday);
-            }
+        const addPeopleForm = document.querySelector('.add');
+
+        // Retrieve birthday from local storage
+        const storedType = localStorage.getItem('accountType');
+        setType(storedType);
+        const storedBirthday = localStorage.getItem('birthday');
+        setBirthday(storedBirthday);
+        const storedFirstName = localStorage.getItem('firstName');
+        setFirstName(storedFirstName);
+        const storedLastName = localStorage.getItem('lastName');
+        setLastName(storedLastName);
+        const storedPhone = localStorage.getItem('phone');
+        setPhone(storedPhone);
+        const storedEmail = localStorage.getItem('email');
+        setEmail(storedEmail);
+        const storedAddressOne = localStorage.getItem('addressOne');
+        setAddressOne(storedAddressOne);
+        const storedAddressTwo = localStorage.getItem('addressTwo');
+        setAddressTwo(storedAddressTwo);
+        const storedPostal = localStorage.getItem('postal');
+        setPostal(storedPostal);
+        if(storedType == "Child"){
+            const storedParentBirthday = localStorage.getItem('parentBirthday');
+            setParentBirthday(storedParentBirthday);
+        }
+
+    // }, []);
+
+    // useEffect(() => {
+        if (addPeopleForm) {
+            addPeopleForm.addEventListener('submit', (e) => {
+                if(type == "Child"){
+                    e.preventDefault();
+                    addDoc(colRef, {
+                        accountType: storedType,
+                        childDateOfBirth: storedBirthday,
+
+                        guardianFirstName: storedFirstName,
+                        guardianLastName: storedLastName,
+                        guardianPhone: storedPhone,
+                        guardianEmail: storedEmail,
+                        guardianAddressOne: storedAddressOne,
+                        guardianAddressTwo: storedAddressTwo,
+                        guardianPostal: storedPostal,
+
+                        guardianDateOfBirth: storedParentBirthday,
+
+                        consentToCommunications: addPeopleForm.consentToCommunications.value,
+                        consentToRules: addPeopleForm.consentToRules.value
+                    }).then(() => {
+                        addPeopleForm.reset();
+                    });
+                }
+                if(type == "Adult"){
+                    e.preventDefault();
+                    addDoc(colRef, {
+                        accountType: storedType,
+                        dateOfBirth: storedBirthday,
+
+                        firstName: storedFirstName,
+                        lastName: storedLastName,
+                        phone: storedPhone,
+                        email: storedEmail,
+                        addressOne: storedAddressOne,
+                        addressTwo: storedAddressTwo,
+                        postal: storedPostal,
+
+                        consentToCommunications: addPeopleForm.consentToCommunications.value,
+                        consentToRules: addPeopleForm.consentToRules.value
+                    }).then(() => {
+                        addPeopleForm.reset();
+                    });
+                }
+            });
         }
     }, []);
 
     const handleFormSubmit = async (e) => {
+        const addPeopleForm = document.querySelector('.add');
+
         e.preventDefault();
         
         const formData = new FormData(e.target);
--- src/app/(pages)/form/layout.jsx
+++ src/app/(pages)/form/layout.jsx
@@ -36,25 +36,15 @@ export default function FormLayout({ children }) {
       <section id="formSiding">
         <div id="formInfoHeading">
           <div className="relative">
-            <h1 className="contestInfo evilInfo" aria-hidden="true">CONTEST INFO</h1>
+            {/* <h1 className="contestInfo evilInfo" aria-hidden="true">CONTEST INFO</h1> */}
             <h1 className="contestInfo ">CONTEST INFO</h1>
           </div>
           <button id="formInfoPopupButton">i</button>
         </div>
-        <div id="formInfoPopup">
-          <div className="formSidingContainer">
-            <p className="paragraphText">
-              1. Fill out the form with your details.<br/>
-              2. Spin the wheel and see if you're a winner!<br/>
-              3. Winners will be notified by email and must correctly answer a skill-testing question to claim their prize.<br/>
-            </p><br/>
-            <p className="paragraphText">
-            Winners will be selected by random draw.* BuyMore Dollars Inc. reserves the right to remove any entries at their discretion.* By entering, you agree to the contest rules and regulations and consent to receive communications from BuyMore Dollars and our sponsors.            </p>
-          </div>
-          <div id="formSidingBodyParagraph">
-            <p className="paragraphText bottom">
-            *Prizes are subject to adherent contest rules and regulations. Delivery timelines may vary, with an estimated arrival of 60-90 days.            </p>
-          </div>
+
+        <div id="formSidingBodyParagraph">
+          <p className="paragraphText bottom">
+          *Prizes are subject to adherent contest rules and regulations.Delivery timelines may vary, with an estimated arrival of 60-90 days          </p>
         </div>
         <div id='formSidingDesk'>
           <div className="formSidingContainer">
