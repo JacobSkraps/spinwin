aafab48f547680de04b713d414b19fb10fcf41e0
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-14 10:32:51 -0500

    Base template for new pages

---
+++ src/app/(pages)/(game)/outcome/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return(
+        <div className="GameSplit">
+            <h1>Loading</h1>
+            <div className="GameInteract"></div>
+            <div className="OutcomeDisplay">
+                <h1>Loading...</h1>
+
+            </div>
+        </div>
+    );
+}
