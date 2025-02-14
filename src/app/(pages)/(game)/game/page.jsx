9cac163d69ad23bfdd90e132baeb2970d6f57e6b
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-14 12:03:57 -0500

    Readded Game template and updated responsiveness for form

---
+++ src/app/(pages)/(game)/game/page.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+    return(
+        <div className="GameSplit">
+            <div className="GameInteract">
+                <h1 id="contestInfo">Let's Play</h1>
+                <div className="GameIntructionsButton">
+                    <h2>Intructions</h2>
+                </div>
+                <h2>Good Luck!</h2>
+            </div>
+            <div id="GameGame">
+                <div id="wheel">
+                    <p>Wheel</p>
+                </div>
+            </div>
+        </div>
+    );
+}
