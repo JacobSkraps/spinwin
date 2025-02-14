561af4b4082888dce48976b2cd627600ff278d2c
Charlize San Gabriel <charlizesangabriel@gmail.com>
2025-02-13 19:25:48 -0500

    Fixed the CSS, classes, and the mobile version

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 //* Remove the output and the basePath if it doesn't work when we run dev. heeehee. this was just for exporting the static site
 const nextConfig = {
-    output: 'export',
-    basePath: isProd ? '/spinwintest' : '',
+    // output: 'export',
+    // basePath: isProd ? '/spinwintest' : '',
 };
 
 export default nextConfig;
