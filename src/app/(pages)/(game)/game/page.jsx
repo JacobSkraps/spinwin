9e10809a260524b61d985844c0189be6040c7f42
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-18 10:06:32 -0500

    The Wheel won't appear!?

--- src/app/(pages)/(game)/game/page.jsx
+++ src/app/(pages)/(game)/game/page.jsx
@@ -1,19 +1,20 @@
-import wheelComponent from "@/app/components/wheel";
+// import WheelComponent from "@/app/components/wheel";
+import { WheelComponent } from "@/../public/wheel.jsx";
 import { gsap } from 'gsap';
 
 export default function Home() {
     return(
         <div className="GameSplit">
             <div className="GameInteract">
-                <h1 id="contestInfo">Let's Play</h1>
+                <h1 className="contestInfo" id="GameHeader">Let's Play</h1>
                 <div className="GameIntructionsButton">
-                    <h2>Intructions</h2>
+                    <h2 className="contestInfo" id="GameIntructionsClickText">Intructions</h2>
                 </div>
-                <h2>Good Luck!</h2>
+                <h2 className="formHeading" id="GoodLuck">Good Luck!</h2>
             </div>
             <div id="GameGame">
                 <div id="wheel">
-                    <wheelComponent></wheelComponent>
+                    <img src={ WheelComponent } alt="" />
                 </div>
             </div>
         </div>
