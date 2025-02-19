4f406e398ee228eea73c6631dee6baf2542fb034
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-18 23:23:06 -0500

    Added Outcome Probability

--- src/app/(pages)/(game)/game/page.jsx
+++ src/app/(pages)/(game)/game/page.jsx
@@ -4,9 +4,42 @@ import WheelComponent from "@/app/components/wheel.jsx";
 // import WheelComponent from "/public/wheel.svg";
 import Image from 'next/image';
 import { gsap } from 'gsap';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [result, setResult] = useState(null);
+
+    const outcomes = [
+        { win: false,
+        type: "Coupon" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "20" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "100" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "20" },
+        { win: false,
+        type: "Coupon" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "750" },
+        { win: false,
+        type: "Coupon" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "20" },
+        { win: false,
+        type: "Coupon" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "100" },
+        { win: true,
+        type: "BuyMoreDollars",
+        value: "10000" }
+    ];
         useEffect(() => {
             gsap.set(spinningPart, {
                 transformOrigin: "50% 50%"
@@ -15,18 +48,47 @@ export default function Home() {
                 gsap.to(spinningPart, {
                     rotation: 360,
                     duration: 4,
-                    ease: "power4.out"
+                    ease: "power4.out",
+                    onComplete: () => {
+                        randomOutcome = Math.random();
+                        let outcome = null
+                        if (randomOutcome < 0.099){
+                            outcome = outcomes[0]
+                        }
+                        else if (randomOutcome < 0.198){
+                            outcome = outcomes[1]
+                        }
+                        else if (randomOutcome < 0.297){
+                            outcome = outcomes[2]
+                        }
+                        else if (randomOutcome < 0.396){
+                            outcome = outcomes[3]
+                        }
+                        else if (randomOutcome < 0.495){
+                            outcome = outcomes[4]
+                        }
+                        else if (randomOutcome < 0.594){
+                            outcome = outcomes[5]
+                        }
+                        else if (randomOutcome < 0.693){
+                            outcome = outcomes[6]
+                        }
+                        else if (randomOutcome < 0.792){
+                            outcome = outcomes[7]
+                        }
+                        else if (randomOutcome < 0.951){
+                            outcome = outcomes[8]
+                        }
+                        else if (randomOutcome < 0.998){
+                            outcome = outcomes[9]
+                        }
+                        else{
+                            outcome = outcomes[10]
+                        }
+                        setResult(outcome);
+                    }
                 });
             });
-                return () => {
-                wheel.removeEventListener('click', () => {
-                    gsap.to(spinningPart, {
-                        rotation: 360,
-                        duration: 1,
-                        ease: "power2.inOut"
-                    });
-                });
-            };
         },[]); 
 
     return(
