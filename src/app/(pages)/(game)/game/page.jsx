31da985ea096d6902598abb7662d6fb73bfa6cb7
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-20 22:26:54 -0500

    WHY WONT IT OWRK

--- src/app/(pages)/(game)/game/page.jsx
+++ src/app/(pages)/(game)/game/page.jsx
@@ -43,61 +43,32 @@ export default function Game() {
     const [rich, setRich] = useState("");
     const [docSnap, setDocSnap] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
+        const checkDocs = async () => {
+            if (userID) {
+                const docRef = doc(db, 'formData', userID);
+                const docSnap = await getDoc(docRef);
+                setDocSnap(docSnap);
+    
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data());
+                    let docTimeSnap = docSnap.data().timeOut;
+                    setDocTime(docTimeSnap);
+                } else {
+                    console.log("no data");
+                }
+            }
+        }
+    
+        checkDocs();
+
+        wheel.addEventListener('click', spinWheel);
+
         const getUserID = localStorage.getItem("userID");
         setUserID(getUserID);
         const getRich = localStorage.getItem("rich");
         setRich(getRich)
-    })
-
-
-    useEffect(()=>{
-        const checkDocs = async () => {
-            if(userID){
-                const docRef = doc(db, 'formData', userID)
-                const getDocSnap = getDoc(docRef);
-                setDocSnap(getDocSnap)
-            }
-
-            if(docSnap.exists){
-                console.log("Document data:", docSnap.data());
-                let docTimeSnap = docSnap.data().timeOut;
-                setDocTime(docTimeSnap)
-            } else {
-                console.log("no data")
-            }
-    }
-
-    checkDocs()
-
-    }, [])
-
-
-
-
-
-    const spinWheel = () => {
-        wheel.removeEventListener('click', spinWheel);
-        gsap.set(spinningPart, {
-            transformOrigin: "50% 50%"
-        });
-        let rotationAmount = storedOutcome.toRotate + 1080
-        console.log(`1080 + ${storedOutcome.toRotate} = ${rotationAmount}`)
-        gsap.to(spinningPart, {
-            rotation: rotationAmount,
-            duration: 4,
-            ease: "power1.out",
-            onComplete: () => {
-                // let randomOutcome = Math.random();
-                localStorage.setItem('outcomeWin', storedOutcome.win);
-                localStorage.setItem('outcomeValue', storedOutcome.value);
-
-                redirect(`/outcome`)
-            }
-        });
-    }
-    useEffect(() => {
-        wheel.addEventListener('click', spinWheel);
+        console.log(rich);
 
         console.log(document);
         let now = Date.now()
@@ -114,7 +85,10 @@ export default function Game() {
                 css:{ 'filter': 'grayscale(100%)'}
             });    
         }
-        
+        // const storedRich = localStorage.getItem('rich');
+        // setRich(storedRich);
+        // console.log(rich);
+
         gsap.set(spinningPart, {
             transformOrigin: "50% 50%"
         });
@@ -205,8 +179,10 @@ export default function Game() {
         }
 
         // const regexResult = regexCheck(docStreet, )
+        console.log(rich);
         if (rich == true){
             storedOutcome = outcomes[10]
+            console.log("right this way sir")
         }
         console.log(storedOutcome);
         console.log(storedOutcome.win);
@@ -217,7 +193,158 @@ export default function Game() {
         // console.log(win);
         // console.log(value);
         // console.log(rotate);
-    },[]); 
+    
+    
+    }, [userID]);
+    
+    
+
+
+
+
+
+    const spinWheel = () => {
+        wheel.removeEventListener('click', spinWheel);
+        gsap.set(spinningPart, {
+            transformOrigin: "50% 50%"
+        });
+        let rotationAmount = storedOutcome.toRotate + 1080
+        console.log(`1080 + ${storedOutcome.toRotate} = ${rotationAmount}`)
+        gsap.to(spinningPart, {
+            rotation: rotationAmount,
+            duration: 4,
+            ease: "power1.out",
+            onComplete: () => {
+                // let randomOutcome = Math.random();
+                localStorage.setItem('outcomeWin', storedOutcome.win);
+                localStorage.setItem('outcomeValue', storedOutcome.value);
+
+                redirect(`/outcome`)
+            }
+        });
+    }
+    // useEffect(() => {
+    //     wheel.addEventListener('click', spinWheel);
+
+    //     console.log(document);
+    //     let now = Date.now()
+    //     if (now < docTime) {
+    //         wheel.removeEventListener('click', spinWheel);
+    //         console.log("You have to wait!");
+    //         // let paraChange = document.querySelector("#GoodLuck");
+    //         // let changingText = "Please come back later to try again.";
+    //         // paraChange.innerText = changingText;
+    //         wheel.style.cursor = "default";
+    //         gsap.to(wheel, {
+    //             duration: 1,
+    //             ease: "power1.out",
+    //             css:{ 'filter': 'grayscale(100%)'}
+    //         });    
+    //     }
+        
+    //     gsap.set(spinningPart, {
+    //         transformOrigin: "50% 50%"
+    //     });
+    //     let randomOutcome = Math.random();
+    //     const outcomes = [
+    //         { win: false,
+    //         type: "Coupon",
+    //         toRotate: 10,
+    //         value: 0 },
+    //         { win: false,
+    //         type: "Coupon",
+    //         toRotate: 88,
+    //         value: 0 },
+    //         { win: false,
+    //         type: "Coupon",
+    //         toRotate: 157,
+    //         value: 0 },
+    //         { win: false,
+    //         type: "Coupon",
+    //         toRotate: 228,
+    //         value: 0 },
+
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 122,
+    //         value: 20 },
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 262,
+    //         value: 20 },
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 334,
+    //         value: 20 },
+
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 45,
+    //         value: 100 },
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 298,
+    //         value: 100 },
+
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 192,
+    //         value: 750 },
+
+    //         { win: true,
+    //         type: "BuyMoreDollars",
+    //         toRotate: 30,
+    //         value: 10000, }
+    //     ];
+    //     // let storedOutcome = {}
+    //     if (randomOutcome < 0.24){
+    //         storedOutcome = outcomes[0]
+    //     }
+    //     else if (randomOutcome < 0.48){
+    //         storedOutcome = outcomes[1]
+    //     }
+    //     else if (randomOutcome < 0.72){
+    //         storedOutcome = outcomes[2]
+    //     }
+    //     else if (randomOutcome < 0.97){
+    //         storedOutcome = outcomes[3]
+    //     }
+    //     else if (randomOutcome < 0.975){
+    //         storedOutcome = outcomes[4]
+    //     }
+    //     else if (randomOutcome < 0.98){
+    //         storedOutcome = outcomes[5]
+    //     }
+    //     else if (randomOutcome < 0.985){
+    //         storedOutcome = outcomes[6]
+    //     }
+    //     else if (randomOutcome < 0.99125){
+    //         storedOutcome = outcomes[7]
+    //     }
+    //     else if (randomOutcome < 0.9975){
+    //         storedOutcome = outcomes[8]
+    //     }
+    //     else if (randomOutcome < 0.999){
+    //         storedOutcome = outcomes[9]
+    //     }
+    //     else{
+    //         storedOutcome = outcomes[10]
+    //     }
+
+    //     // const regexResult = regexCheck(docStreet, )
+    //     if (rich == true){
+    //         storedOutcome = outcomes[10]
+    //     }
+    //     console.log(storedOutcome);
+    //     console.log(storedOutcome.win);
+    //     console.log(storedOutcome.value);
+    //     // setWin(storedOutcome.win);
+    //     // setValue(storedOutcome.value);
+    //     // setRotate(storedOutcome.toRotate);
+    //     // console.log(win);
+    //     // console.log(value);
+    //     // console.log(rotate);
+    // },[]); 
 
     return(
         <div className="GameSplit">
