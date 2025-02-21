a9204c0d759ca2057fe2dc4eb5261994bbf7fd2b
JacobSkraps <Sparks.Workx@gmail.com>
2025-02-20 22:59:24 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/spinwin',
+  // output: 'export',
+  // basePath: '/spinwin',
   images: {
     unoptimized: true,
   },
